98a5960f83a3219d6d6eb80de352ec38ac083a96
Williams <williams.dmca@gmail.com>
2022-09-11 00:06:35 -0300

    avance

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <div className="m-5">
+      <li><Link to={'/'}>Mi app</Link></li>
+    </div>
+  )
+}
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
+
+
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import ItemDetailContainer from './components/shop/ItemDetailContainer';
+import ItemListContainer from './components/shop/ItemListContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ //<React.StrictMode>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/shop' element={<ItemListContainer/>}/>
+      <Route path='/shop/item/:slug' element={<ItemDetailContainer/>}/>
+    </Routes>
+    <Footer/>
+  </BrowserRouter>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
